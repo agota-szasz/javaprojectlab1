cf7fe261d080355491ace3a213db8dfc69ba2c84
szasz-agota <agota06@yahoo.com>
2021-04-03 17:57:23 +0300

    modificare pe fisierul main

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,40 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Exzercise no.1
+        System.out.println("Exercise no.1");
         System.out.println("Hello world");
         System.out.println("Agota");
 
-        int x = 3;
-        x ++;
-        System.out.println(x++);
-        System.out.println(x);
-        System.out.println(x);
-        x-=2;
-        System.out.println(x);
+     // Exzercise no.2
+        System.out.println("Exercise no.2 = " +(10 + 1));
 
-        System.out.println(x%2 == 0);
-        System.out.println();
+     // Excercise no.3
+        float mere = 8;
+        float copii = 3;
+        System.out.println("Exercise no.3 ="+( mere/ copii));
+
+     // Exercise no.4 (a)
+        System.out.println("Exercise no.4");
+        System.out.println("Exercise no.4 (a) = " + (-5+8*6));
+
+     // Exercise no.4 (b)
+        int x=55;
+        int y=9;
+        System.out.println("Exercise no.4 (b) ="+(x+y)%y);
+
+     // Exercise no.4 (c)
+        float a=20;
+        float b=3;
+        float c=5;
+        float d=8;
+        System.out.println("Exercise no.4 (c) =" +(a+(-b*c)/d));
+
+     // Exercise no.4 (d)
+        System.out.println("Exercise no.4 (d) =" +(5+15/3*2-8%3));
+
+
+
 
 
 
