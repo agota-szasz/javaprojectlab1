49458eeea31c427e08e578ace6a94d00001244c6
szasz-agota <agota06@yahoo.com>
2021-04-05 08:44:54 +0300

    modificare pe fisierul lab2

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -57,7 +57,7 @@ public class Main {
         System.out.println("  |       ^         | " );
         System.out.println("  |     '____'      | " );
         System.out.println("  +                 + " );
-        System.out.println("   ------------------  ");
+        System.out.println("   -----------------  " );
 
 
     }
